8d144b25b0be7bc6bd26a36ba0c37d84e148bca5
RaffaeleArbues <raffaelearbues02@gmail.com>
2026-07-04 13:55:53 +0200

    solchi first commit

---
+++ backend/src/main/java/it/solchi/SolchiApplication.java
@@ -0,0 +1,13 @@
+package it.solchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolchiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/solchi/SolchiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.solchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
